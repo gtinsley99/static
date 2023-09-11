4478308ff5bf661b4c7b21c93e151fa9525b436a
George Tinsley <george8s@live.co.uk>
2023-09-11 14:44:16 +0100

    db connection

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const app = express();
 // Allows us to decide which port number to assign
 const port = 5001;
 
+
+
 // express.static("public") sets up a static webserver pulling files from the public folder
-// app.use creates a route so that when we type in localhost:5001/public it follows this route 
-app.use("/public", express.static("public"));
+// app.use creates a route so that when we type in localhost:5001/public it follows this route
+// app.use("/public", express.static("public"));
 
 // This is the listener function which only listens into http request on port 5001, the anonymous function runs just once when the listener is set up
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
